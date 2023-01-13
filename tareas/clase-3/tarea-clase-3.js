941b9c8abc322c75ce9bee101c8a373e266ab87c
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2023-01-13 18:47:19 -0300

    Crea las tareas de clase 3

--- tareas/clase-3/tarea-clase-3.js
+++ tareas/clase-3/tarea-clase-3.js
@@ -19,3 +19,44 @@
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+const miNombre = 'Gastón';
+const nombrePadre = 'Adolfo';
+
+const nombreUsuario = prompt('Cómo te llamás?');
+
+if (nombreUsuario === miNombre) {
+    alert(`Hola, Tocayo! Yo también me llamo ${nombreUsuario}`);
+} else if (nombreUsuario === nombrePadre) {
+    alert(`Hola ${nombreUsuario}, te llamás igual que mi papá`)
+} else {
+    alert(`Hola ${nombreUsuario}`);
+};
+
+
+const edadUsuario = Number(prompt('Cuantos años tenés?'));
+const miEdad = 41;
+
+if (edadUsuario === miEdad) {
+    alert('Tenemos la misma edad');
+} else if (edadUsuario < miEdad) {
+    alert('Sos más joven que yo');
+} else {
+    alert('Tenés más años que yo')
+};
+
+
+const tieneDocumento = prompt('Hola, tenés documento?');
+
+if (tieneDocumento.toLowerCase() === 'si') {
+    const edadUsuarioBar = Number(prompt('Cuántos años tenés?'));
+    if (edadUsuarioBar >= 18) {
+        alert('Sos mayor de edad, podés entrar al bar');
+    } else {
+        alert('Sos menor de edad, no podés entrar al bar');
+    }
+} else if (tieneDocumento.toLowerCase() === 'no'){
+    alert ('Sin documentos no podés entrar al bar');
+} else {
+    alert('No entendí la respuesta');
+}
