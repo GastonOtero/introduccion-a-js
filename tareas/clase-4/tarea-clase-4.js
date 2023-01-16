3c33f517a1df5fb91684fb609828351ed12bc9ab
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2023-01-16 12:20:08 -0300

    Crea tareas de clase 4

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,8 +1,16 @@
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
+for (let i = 3; i <= 22; i+=3) {
+    console.log(i);
+}
 
 
 // TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
 //       diez a uno.
+let i = 10;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
 
 
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
@@ -17,5 +25,29 @@
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+function fizzBuzz(){
+    for (let i = 1; i <= 50; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log("Fizz");
+        } else if(i % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(i)
+        }
+    }
+}
+fizzBuzz();
+
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
+
+function calcularPromedio(array){
+    let sumaElementosArray = 0;
+    for (let i = 0; i < array.length; i++){
+        sumaElementosArray += array[i];
+    };
+    return sumaElementosArray / array.length;
+}
+calcularPromedio([10,5,4,2,8]);
